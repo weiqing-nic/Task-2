c4f2f1632265e2c93c2ebd512ca95b640683fb58
weiqing-nic <yoweiqing@gmail.com>
2022-07-12 16:27:21 +0800

    Add files via upload

---
+++ DBContext/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskProcessor.Models;
+
+namespace TaskProcessor.DBContext
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskClass> Task { get; set; }
+    }
+}
---
+++ Models/TaskClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskProcessor.Models
+{
+    public class TaskClass
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
